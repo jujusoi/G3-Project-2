31ff78f5ab12b7e9e0d4be2c872c80e8bb5673b7
jujusoi <jalxmcdonald@hotmail.com>
2023-09-13 12:32:14 +0930

    added server start code, needs connectionjs and env to start

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exprhb = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 2423;
+
+const sess = {
+    secret: 'adnjoaewoiew',
+    cookie: {
+        maxAge: 60 * 60 * 2000, // will expire in 2 hours
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exprhb.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server up at http://localhost:${PORT}`));
+});

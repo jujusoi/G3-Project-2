701e2d35ca07360caeac5d313d4214f724e46c88
jujusoi <jalxmcdonald@hotmail.com>
2023-09-14 19:55:40 +0930

    started adding models

---
+++ models/Wishlist.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Wishlist extends Model{};
+
+Wishlist.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+    book_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'book',
+            key: 'id',
+        },
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'wishlist'
+});
+
+module.exports = Wishlist;

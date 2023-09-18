9cfbf18146db65ea1121c7e3f27008cae35a5d06
Charlton Ortega <charltonortega14@gmail.com>
2023-09-18 21:29:26 +0930

    DO NOT MERGE - please fix api routes for homepage search integration homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,11 +1,24 @@
-const bookClick = document.querySelector('#book-table-ting');
+const bookClick = document.querySelector("#book-table-ting");
 
-bookClick.addEventListener('click', function(event) {
-    const target = event.target;
-    if (target.hasAttribute('data-bookid')) {
-        const id = target.getAttribute('data-bookid');
-        location.href = `/home/books/${id}`;
-    } else {
-        return;
-    };
+bookClick.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.hasAttribute("data-bookid")) {
+    const id = target.getAttribute("data-bookid");
+    location.href = `/home/books/${id}`;
+  } else {
+    return;
+  }
 });
+
+// clear filter button
+const bookSearchForm = document.getElementById("bookSearchForm");
+const clearFiltersButton = document.getElementById("clearFilters");
+
+// Function to clear form fields
+function clearFilters() {
+  // Reset the form to its initial state
+  bookSearchForm.reset();
+}
+
+// Attach a click event listener to the clear button
+clearFiltersButton.addEventListener("click", clearFilters);

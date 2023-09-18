679a5e6e1537084c9fdb043695638818b74a1df5
jujusoi <jalxmcdonald@hotmail.com>
2023-09-18 22:44:26 +0930

    added wishlist create and save functionality, as well as delete

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const book = require('./books');
 const categories = require('./tags');
-const { Books, Categories } = require('../../models');
+const { Books, Categories, Wishlist } = require('../../models');
 const handlebars = require('handlebars');
 
 handlebars.registerHelper("increment", function(val, options){
     return parseInt(val) + 1;
+});
+
+handlebars.registerHelper('inWishlist', function(id, wishlist) {
+    return wishlist.some(wishItem => wishItem.book_id === id);
 });
 
 router.get('/', async (req, res) => {
@@ -15,10 +19,22 @@ router.get('/', async (req, res) => {
             order: [[ 'id', 'ASC']],
             limit: 20,
         });
+        const wishlistData = await Wishlist.findAll({
+            where: {
+                user_id: req.session.user.id,
+            },
+        });
+    const mappedWishlist = wishlistData.map((wish) => wish.get({ plain: true }));
+
     const mappedData = bookData.map((book) => book.get({ plain: true }));
+    mappedData.forEach(book => {
+        book.wishlistStuff = mappedWishlist
+    });
+
+    console.log(mappedData);
     if (mappedData) {
         res.status(200).render('book-display', {
-            mappedData
+            mappedData, mappedWishlist
         })
     } else {
         res.status(400).json({ message: `Could not return books`});
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -9,3 +9,36 @@ bookClick.addEventListener('click', function(event) {
         return;
     };
 });
+
+bookClick.addEventListener('click', async function(event) {
+    const target = event.target;
+    if (target.hasAttribute('data-wish-bookid') && target.classList.contains('star-unfilled')) {
+        target.classList.remove('star-unfilled');
+        target.classList.add('star-filled');
+        const id = target.getAttribute('data-wish-bookid');
+        const response = await fetch(`/api/wishlist/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({ id }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+        if (response.ok) {
+            console.log(`Sent req`);
+        } else {
+            alert(`Could not save`);
+        }
+    } else if (target.hasAttribute('data-wish-bookid') && target.classList.contains('star-filled')) {
+        target.classList.remove('star-filled');
+        target.classList.add('star-unfilled');
+        const id = target.getAttribute('data-wish-bookid');
+        const response = await fetch(`/api/wishlist/${id}`, {
+            method: 'DELETE',
+            body: JSON.stringify({ id }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+        if (response.ok) {
+            console.log(`Sent req`);
+        } else {
+            alert(`Could not delete`);
+        }
+    }
+});

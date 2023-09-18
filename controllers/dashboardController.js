a2143c87d12a6f32f8554f1e94f4cd5019f1e4f7
jujusoi <jalxmcdonald@hotmail.com>
2023-09-18 10:28:36 +0930

    added basic homepage book renders with example click hrefs

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 router.get("/dashboard", (req, res) => {
   if (req.session.isAuthenticated) {
-    res.render("dashboard", { user: req.session.user }); // Pass the user data to the dashboard view
+    
+    res.render("dashboard", { user: req.session.user,
+      
+     }); // Pass the user data to the dashboard view
   } else {
     res.redirect("/login"); // Redirect to login if the user is not authenticated
   }
---
+++ controllers/home/index.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Books, Categories } = require('../../models');
+const handlebars = require('handlebars');
+
+handlebars.registerHelper("increment", function(val, options){
+    return parseInt(val) + 1;
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const bookData = await Books.findAll({
+            include: [{ model: Categories }],
+            order: [[ 'id', 'ASC']],
+            limit: 20,
+        });
+    const mappedData = bookData.map((book) => book.get({ plain: true }));
+    if (mappedData) {
+        res.status(200).render('book-display', {
+            mappedData
+        })
+    } else {
+        res.status(400).json({ message: `Could not return books`});
+    }
+    } catch (err) {
+        res.status(500).json({ message: `Failed to get book page`});
+    }
+});
+
+module.exports = router;

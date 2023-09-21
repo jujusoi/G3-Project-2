b2add87564733764c697ddbc00d6747c6f8939b3
jujusoi <jalxmcdonald@hotmail.com>
2023-09-21 13:37:49 +0930

    added delete button. need image and stuff

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -15,30 +15,49 @@ comment.post('/', async (req, res) => {
     }
 });
 
+comment.delete('/:id', async (req, res) => {
+    try {
+        const deleteData = await Reviews.destroy({
+            where: {
+                id: req.body.reviewId,
+            }
+        });
+        await updateScore(req, res, true);
+    } catch (err) {
+        res.status(500).json({ message: `Could not send delete req, ${err}`});
+    };
+});
+
 comment.put('/:id', async (req, res) => {
     try {
-        const bookData = await Books.findByPk(req.params.id, {
-            include: {
-                model: Reviews,
-            },
-        });
-        let finalScore = 0;
-        bookData.reviews.forEach(review => {
-            finalScore += review.score_given;
-        });
-        const newAvg = finalScore / bookData.reviews.length;
-        bookData.set({
-            average_score: newAvg.toFixed(1),
-        });
-        await bookData.save();
-        if (bookData) {
-            res.status(200).json({ message: 'Updated book score'});
-        } else {
-            res.status(404).json({ message: 'could not find book'});
-        };
+        await updateScore(req, res, false);
     } catch (err) {
         res.status(500).json({ message: `Could not make PUT request, ${err}`});
     };
 });
+
+const updateScore = async (req, res, check) => {
+    const bookData = await Books.findByPk(req.params.id, {
+        include: {
+            model: Reviews,
+        },
+    });
+    let finalScore = 0;
+    bookData.reviews.forEach(review => {
+        finalScore += review.score_given;
+    });
+    const newAvg = finalScore / bookData.reviews.length;
+    bookData.set({
+        average_score: newAvg.toFixed(1),
+    });
+    await bookData.save();
+    if (bookData && !check) {
+        res.status(200).json({ message: 'Updated book score'});
+    } else if (bookData && check) {
+        res.status(200).json({ message: 'Successfully deleted comment!'});
+    } else {
+        res.status(404).json({ message: 'could not find book'});
+    }
+}
 
 module.exports = comment;
--- controllers/home/books.js
+++ controllers/home/books.js
@@ -1,5 +1,11 @@
 const book = require('express').Router();
 const { User, Books, Categories, Reviews } = require('../../models');
+const handlebars = require('handlebars');
+
+handlebars.registerHelper("equals", function(val1, val2){
+    return val1 === val2;
+});
+
 
 book.get('/:id', async (req, res) => {
     try {
@@ -24,6 +30,7 @@ book.get('/:id', async (req, res) => {
             const userInfo = req.session.user;
             const serializedData = bookData.get({plain: true});
             const numberOfReviews = serializedData.reviews.length;
+            console.log(serializedData);
             res.status(200).render('unique-book', { 
                 serializedData, userInfo, numberOfReviews },
             );
@@ -34,5 +41,7 @@ book.get('/:id', async (req, res) => {
         res.status(500).json({ message: `Get request failed, ${err}`});
     };
 });
+
+
 
 module.exports = book;

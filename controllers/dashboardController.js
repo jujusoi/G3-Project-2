bad39a020498734c889381f051c3ca5c5192b985
Charlton Ortega <charltonortega14@gmail.com>
2023-09-23 13:45:36 +0930

    implement edit and delete for user accounts

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,36 +1,90 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../config/middleware/auth');
-const { Wishlist, Books, Categories, User} = require('.//../models');
+const auth = require("../config/middleware/auth");
+const { Wishlist, Books, Categories, User } = require(".//../models");
 
-router.get("/dashboard", auth, async (req, res) => {    
-    try {
-        const wishlistData = await Wishlist.findAll({
-            where:{
-                user_id: req.session.user.id,
-            }, 
-            include: [
-                {
-                    model: Books, include: [{
-                        model: Categories,
-                    }],
-                },
-            ],
-        });
-        const userStuff = await User.findOne({
-            where: {
-                id: req.session.user.id,
-            }, attributes: {
-                exclude: ['password'],
+router.get("/dashboard", auth, async (req, res) => {
+  try {
+    const wishlistData = await Wishlist.findAll({
+      where: {
+        user_id: req.session.user.id,
+      },
+      include: [
+        {
+          model: Books,
+          include: [
+            {
+              model: Categories,
             },
-        });
-        const userDate = userStuff.created_at;
-        const mappedWishlist = wishlistData.map((wishlist) => wishlist.get({ plain: true }));
-        res.render("dashboard", { title: 'My Account', userInfo: req.session.user, mappedWishlist, userDate,
+          ],
+        },
+      ],
+    });
+    const userStuff = await User.findOne({
+      where: {
+        id: req.session.user.id,
+      },
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    const userDate = userStuff.created_at;
+    const mappedWishlist = wishlistData.map((wishlist) =>
+      wishlist.get({ plain: true })
+    );
+    res.render("dashboard", {
+      title: "My Account",
+      userInfo: req.session.user,
+      mappedWishlist,
+      userDate,
     }); // Pass the user data to the dashboard view
-    } catch (err) {
-        res.status(500).json({message : `Could not make request, ${err}`});
+  } catch (err) {
+    res.status(500).json({ message: `Could not make request, ${err}` });
+  }
+});
+
+// PUT route for updating the username
+router.put("/api/users/updateUsername", auth, async (req, res) => {
+  try {
+    const { newUsername } = req.body;
+
+    // Check if the new username is valid (e.g., not empty)
+    if (!newUsername || newUsername.trim() === "") {
+      return res.status(400).json({ message: "Invalid username" });
     }
+
+    // Update the username in the database
+    await User.update(
+      { username: newUsername },
+      {
+        where: {
+          id: req.session.user.id,
+        },
+      }
+    );
+
+    res.status(200).json({ message: "Username updated successfully" });
+  } catch (err) {
+    res.status(500).json({ message: `Could not update username, ${err}` });
+  }
+});
+
+// DELETE route for deleting the account
+router.delete("/api/users/deleteAccount", auth, async (req, res) => {
+  try {
+    // Delete the user's account from the database
+    await User.destroy({
+      where: {
+        id: req.session.user.id,
+      },
+    });
+
+    req.session.destroy(() => {
+      res.status(200).json({ message: "Account deleted successfully" });
+    });
+  } catch (err) {
+    res.status(500).json({ message: `Could not delete account, ${err}` });
+  }
 });
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,28 +1,88 @@
-const bookClick = document.querySelector('#book-table-ting');
+const bookClick = document.querySelector("#book-table-ting");
 
-
-bookClick.addEventListener('click', function(event) {
-    const target = event.target;
-    if (target.hasAttribute('data-bookid')) {
-        const id = target.getAttribute('data-bookid');
-        location.href = `/home/books/${id}`;
-    } else {
-        return;
-    };
+bookClick.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target.hasAttribute("data-bookid")) {
+    const id = target.getAttribute("data-bookid");
+    location.href = `/home/books/${id}`;
+  } else {
+    return;
+  }
 });
 
-bookClick.addEventListener('click', async function(event) {
-    const target = event.target;
-    if (target.hasAttribute('data-wishlist')) {
-        const id = target.getAttribute('data-wishlist');
-        const response = await fetch(`/api/userwishlist/${id}`, {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json'},
-        });
+bookClick.addEventListener("click", async function (event) {
+  const target = event.target;
+  if (target.hasAttribute("data-wishlist")) {
+    const id = target.getAttribute("data-wishlist");
+    const response = await fetch(`/api/userwishlist/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      window.location.reload();
+    }
+  } else {
+    return;
+  }
+});
+
+// Edit Username
+$("#editBtn").click(function () {
+  // Show the edit username form and hide other elements
+  $("#editUsernameForm").show();
+  $("#editBtn").hide();
+  $("#dltBtn").hide();
+});
+
+$("#editUsernameForm").submit(async function (event) {
+  event.preventDefault();
+  const newUsername = $("#newUsername").val();
+
+  //  PUT request to update the username
+  const response = await fetch("/api/users/updateUsername", {
+    method: "PUT",
+    body: JSON.stringify({ newUsername }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    // Update the user's username
+    $(".fw-bold").text(newUsername);
+
+    // Hide the form and show the username display
+    $("#editUsernameForm").hide();
+    $(".fw-bold").show();
+    $("#editBtn").show();
+    $("#dltBtn").show();
+  } else {
+    // Handle error
+    alert("Failed to update username. Please try again.");
+  }
+});
+
+// DELETE request
+$("#dltBtn").click(function () {
+  const confirmDelete = confirm(
+    "Are you sure you want to delete your account? This action cannot be undone."
+  );
+
+  if (confirmDelete) {
+    fetch("/api/users/deleteAccount", {
+      method: "DELETE",
+    })
+      .then((response) => {
         if (response.ok) {
-            window.location.reload();
+          alert("Account deleted successfully");
+
+          // Redirect page to sign up
+          window.location.href = "/register"; //
+        } else {
+          alert("Failed to delete account");
         }
-    } else {
-        return;
-    };
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+        alert("An error occurred while deleting the account");
+      });
+  }
 });

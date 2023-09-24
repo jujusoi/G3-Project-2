41ed8d11399a6d79d01bfacdbe9e416efe5ae471
jujusoi <jalxmcdonald@hotmail.com>
2023-09-24 12:34:50 +0930

    commit stuff

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,4 @@
+
 const bookClick = document.querySelector("#book-table-ting");
 
 bookClick.addEventListener("click", function (event) {
@@ -27,62 +28,50 @@ bookClick.addEventListener("click", async function (event) {
 });
 
 // Edit Username
-$("#editBtn").click(function () {
-  // Show the edit username form and hide other elements
-  $("#editUsernameForm").show();
-  $("#editBtn").hide();
-  $("#dltBtn").hide();
+// Show the edit username form and hide other elements
+
+document.querySelector('#editBtn').addEventListener('click', function() {
+  document.querySelector('#editUsernameForm').style.display = 'block';
+  document.querySelector('#editBtn').style.display = 'none';
+  document.querySelector('#dltBtn').style.display = 'none';
 });
 
-$("#editUsernameForm").submit(async function (event) {
+
+document.querySelector('#editUsernameForm').addEventListener('submit', async function(event) {
   event.preventDefault();
-  const newUsername = $("#newUsername").val();
-
-  //  PUT request to update the username
-  const response = await fetch("/api/users/updateUsername", {
-    method: "PUT",
+  const newUsername = document.querySelector('#newUsername').value.trim();
+  const response = await fetch('/api/users/updateUsername', {
+    method: 'PUT',
     body: JSON.stringify({ newUsername }),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
-
   if (response.ok) {
-    // Update the user's username
-    $(".fw-bold").text(newUsername);
-
-    // Hide the form and show the username display
-    $("#editUsernameForm").hide();
-    $(".fw-bold").show();
-    $("#editBtn").show();
-    $("#dltBtn").show();
+    document.querySelector('.fw-bold').textContent = newUsername;
+    document.querySelector('#editUsernameForm').style.display = 'none';
+    document.querySelector('.fw-bold').style.display = 'block';
+    document.querySelector('#editBtn').style.display = 'block';
+    document.querySelector('#dltBtn').style.display = 'block';
   } else {
-    // Handle error
-    alert("Failed to update username. Please try again.");
+    alert('Failed to update username. Please try again.');
   }
 });
 
-// DELETE request
-$("#dltBtn").click(function () {
-  const confirmDelete = confirm(
-    "Are you sure you want to delete your account? This action cannot be undone."
-  );
-
+document.querySelector('#dltBtn').addEventListener('click', function() {
+  const confirmDelete = confirm('Are you sure you want to delete your account? This action cannot be undone.');
   if (confirmDelete) {
-    fetch("/api/users/deleteAccount", {
-      method: "DELETE",
-    })
-      .then((response) => {
-        if (response.ok) {
-          alert("Account deleted successfully");
-
-          // Redirect page to sign up
-          window.location.href = "/register"; //
-        } else {
-          alert("Failed to delete account");
-        }
-      })
-      .catch((error) => {
-        console.error("An error occurred:", error);
-        alert("An error occurred while deleting the account");
-      });
+    fetch('/api/users/deleteAccount', {
+      method: 'DELETE',
+    }).then((response) => {
+      if (response.ok) {
+        alert('Account deleted successfully');
+        window.location.href = '/register';
+      } else {
+        alert('Failed to delete account');
+      }
+    }).catch((error) => {
+      console.error('An error occurred:', error);
+      alert('An error occurred while deleting the account');
+    });
   }
 });
+// DELETE request
